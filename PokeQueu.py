779206581f225d96ff66622b11a1b8a8e78852f6
msomarri <57019109+msomarri@users.noreply.github.com>
2022-05-10 13:30:44 -0400

    Update PokeQueu.py

--- PokeQueu.py
+++ PokeQueu.py
@@ -1,20 +1,24 @@
 import pandas as pd
-
 Queue = []
 db = pd.read_csv("pokemon_data.csv")
+
+def add( pknum ):
+    print(pknum)
+    search = db.loc[ db["dex number"] == pknum]
+    print(search)
+
 print( " Welcome to PokeQueue.\nStart out by typing in the dex numbers that you want complete today")
 print("Hit . to quit ")
 done = True
 
 while done:
-    if input() == ".":
+    answer = input()
+    if answer == ".":
         done = False
     # otherwise we got our function here . First starting out by add
-# TODO
+    add( answer)
+#TODO
 # ADD pk to queue
 # rm pk from queue
 # search pk
 
-def add( pknum ):
-    db["dex number"].where()
-print(db)
