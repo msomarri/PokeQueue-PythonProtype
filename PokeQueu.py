5f8dedfaaa77d06b01785b901684f8fc4b4aac5c
msomarri <57019109+msomarri@users.noreply.github.com>
2022-05-18 23:07:39 -0400

    Basic functionally added
    add some basic commands for now

--- PokeQueu.py
+++ PokeQueu.py
@@ -2,28 +2,47 @@ import pandas as pd
 
 Queue = []
 db = pd.read_csv("pokemon_data.csv")
-print(db)
 
+menu = "1: View All Pokemon\n" \
+       "2:Add Pokemon\n" \
+       "3:View current Queue\n" \
+       "4:Search\n" \
+       "5:Quit\n"
 
 # Noramlly this stuff will handle by a db app
 def add(pknum):
     search = db.loc[db['dex number'] == int(pknum)]
-    print(search)
+    if search.empty:
+        print("Pokemon Number does not exist")
+        return
     Queue.append(search)
-
-def remove(pknum):
+def searchPk(pknum):
+    print(db.loc[db['dex number']] == int(pknum))
     
+def viewPokemon():
+    print(db[["dex number","pokemon"]].to_string(index=False))
 
+def viewQueue():
+    print("=====================================")
+    for items in Queue:
+        print(items['pokemon'].to_string())
+    print("=====================================")
 print(" Welcome to PokeQueue.\nStart out by typing in the dex numbers that you want complete today")
-print("Hit . to quit ")
 done = True
 
 while done:
-    answer = input()
-    if answer == ".":
+    answer = input(menu)
+    if answer == "5":
         done = False
-    # otherwise we got our function here . First starting out by add
-    add(answer)
+    if answer == "1":
+        viewPokemon()
+    if answer == "2":
+        add(input("Enter the dex number\n"))
+    if answer == "3":
+        viewQueue()
+    if answer == "4":
+        searchPk(input("Enter the dex number\n"))
+
 # TODO
 # ADD pk to queue
 # rm pk from queue
